c86e0456d290a5a1860941ad83cba96a7cbb90ca
iantunc <iantunc@gmail.com>
2020-05-20 07:16:05 +0300

    HyperStake parameters added

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xdb, 0xad, 0xbd, 0xda };
 
 CMessageHeader::CMessageHeader()
 {
